dbdf679761a0e710beb5964f72a2170fa1251586
MuYvs <MuhammadYvsvf@gmail.com>
2025-03-30 17:52:15 +0200

    Solution for 03_reverseString.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let str_array = string.split("");
+    str_array.reverse();
+    let new_str = str_array.join("")
+    return new_str;
 };
 
 // Do not edit below this line

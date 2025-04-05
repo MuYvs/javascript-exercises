ad1331cdc6ded23bfda203e5c1d5112cad6adf18
MuYvs <MuhammadYvsvf@gmail.com>
2025-04-05 20:29:00 +0200

    Solution for 05_sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(num1, num2) {
     let total = 0;
-    if (num1 > 0 && num2 > 0){
+    if (num1 > 0 && num2 > 0 && Number.isInteger(num1) == true && Number.isInteger(num2) == true){
         if (num1 > num2){
             for (; num2 <= num1; num2++)
                 total += num2;
